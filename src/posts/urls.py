536ffd9cd74bb6dd31e367d9f693074d88fc8ceb
Vinoth G <vin10catchme@gmail.com>
2018-06-27 20:03:02 +0530

    Updating views

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-# import posts
+from django.conf.urls import url
+
+import posts
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    # url(r'^posts/$', views.posts_home),
-    url(r'^posts/', include("posts.urls"))
+    url(r'^$', "posts.views.posts_home"),
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import FileResponse
 
 
 # Create your views here.
+from posts import urls
+
 
 def posts_home(request):
     print("------>")
+    # return FileResponse("urls.py")
     return HttpResponse("<h1>Hello</h1>")
