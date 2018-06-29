69df20c0c62914317bb5798c852129cc5188e99c
Vinoth G <vin10catchme@gmail.com>
2018-06-29 10:35:51 +0530

    Updating views

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,27 +2,34 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import FileResponse
 
-
 # Create your views here.
 from posts import urls
+from .models import Post
+
 
 def posts_list(request):
     print("------>")
+    queryset = Post.objects.all()
+    result = ""
+    for obj in queryset:
+        result += obj.title + "\n"
     context = {}
     if request.user.is_authenticated():
         context = {
-            "Detail": "List",
+            "Detail": result,
+            "queryset": queryset,
             "Name": "Vinoth",
             "Designation": "Software Engineer"
         }
     else:
         context = {
-            "Detail" : "Authentication failed for List",
-            "Name" : "Vinoth",
-            "Designation" : "Software Engineer"
+            "Detail": "Authentication failed for List",
+            "Name": "Vinoth",
+            "Designation": "Software Engineer"
         }
     return render(request, "index.html", context)
     # return HttpResponse("<h1>List</h1>")
+
 
 def posts_create(request):
     if request.user.is_authenticated():
@@ -37,6 +44,7 @@ def posts_create(request):
     # return HttpResponse("<h1>Create</h1>")
     return render(request, "index.html", context)
 
+
 def posts_update(request):
     print("------>")
     if request.user.is_authenticated():
@@ -46,6 +54,7 @@ def posts_update(request):
     # return HttpResponse("<h1>Update</h1>")
     return render(request, "index.html", context)
 
+
 def posts_detail(request):
     print("------>")
     context = {
@@ -53,6 +62,7 @@ def posts_detail(request):
     }
     # return HttpResponse("<h1>Detail</h1>")
     return render(request, "index.html", context)
+
 
 def posts_delete(request):
     print("------>")
